4b1f826ceb0dac463c64faa876ac6fba3866af19
Jaroslav-Pol <73476503+Jaroslav-Pol@users.noreply.github.com>
2021-05-26 10:30:48 +0300

    Initial commit

---
+++ api_example/postit_api/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path, include
+from .views import PostList, PostDetail, CommentList, CommentDetail, PostLikeCreate
+
+urlpatterns = [
+    path('posts', PostList.as_view()),
+    path('posts/<int:pk>', PostDetail.as_view()),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    # path('comments', CommentList.as_view()),
+    path('posts/<int:pk>/comments', CommentList.as_view()),
+    path('comments/<int:pk>', CommentDetail.as_view()),
+    path('posts/<int:pk>/like', PostLikeCreate.as_view()),
+]
